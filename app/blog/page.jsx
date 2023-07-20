0e96a29b99485d99d4e43697d6948bc769b55124
Ohi20 <ashrafohi20@gmail.com>
2023-07-20 12:56:12 +0600

    Folder-File-project-setup

---
+++ app/blog/page.jsx
@@ -0,0 +1,7 @@
+import TopicsList from '@/components/TopicList';
+
+const Blog = () => {
+  return <TopicsList />;
+};
+
+export default Blog;
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,7 @@
+import DashboardList from '@/components/DashboardList';
+
+const Dashboard = () => {
+  return <DashboardList />;
+};
+
+export default Dashboard;
